35c1f729a4f7745cc31d1ba3cd1f6d618bc4153a
J-esper <jespervanstee@gmail.com>
2021-01-29 16:56:59 +0100

    Duplicate detection added

---
+++ groundtruth.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import itertools
+import re
+from matplotlib import pyplot as plt
+from snapy import MinHash, LSH
+
+file = "news_articles_small.csv"
+
+data = pd.read_csv(file)
+
+def get_shingle(k, a):
+    for i in range(0,len(a)-2+1):
+        yield '-'.join(tuple(a[i:i+2]))
+
+def jaccard_similarity(a, b):
+    intersection = len(list(set(a) & set(b)))
+    union = (len(a) + len(b)) - intersection
+    return float(intersection) / float(union)
+
+# Create n-shingles for every article
+n = 2
+data['article'] = data['article'].map(lambda x: [i for i in get_shingle(n, re.sub('[^A-Za-z0-9 ]+', '', x.lower()).split(' '))])
+
+pairs = filter(lambda x: x[0] != x[1], itertools.product(data['article'], data['article']))
+# pairs = itertools.product(data['article'], data['article'])
+sim = []
+for a, b in pairs:
+    sim.append(jaccard_similarity(a, b))
+
+bins = np.arange(0, 1, 0.005)
+
+plt.xlim([0, 1])
+
+plt.hist(sim, bins=bins, alpha=0.5)
+plt.title('Histogram of Jaccard Similarities')
+plt.xlabel('Jaccard Similarity')
+plt.ylabel('count')
+
+plt.show()
---
+++ lsh.py
@@ -0,0 +1,50 @@
+# http://ekzhu.com/datasketch/documentation.html#datasketch.MinHashLSH
+# http://ekzhu.com/datasketch/lsh.html
+
+import pandas as pd
+import numpy as np
+import itertools
+import re
+from snapy import MinHash, LSH
+from datasketch import MinHash, MinHashLSH
+file = "news_articles_small.csv"
+
+data = pd.read_csv(file)
+
+def get_shingle(k, a):
+    for i in range(0,len(a)-2+1):
+        yield '-'.join(tuple(a[i:i+2]))
+
+# Different shingle lengths
+# n = 5
+# data['article'] = data['article'].map(lambda x: [i for i in get_shingle(n, re.sub('[^A-Za-z0-9 ]+', '', x.lower()).split(' '))])
+# Length 1
+data['article'] = data['article'].map(lambda x: re.sub('[^A-Za-z0-9 ]+', '', x.lower()).split(' '))
+
+
+mh_list = []
+num_perm = 256
+for i in range(0, len(data)):
+    mh = MinHash(num_perm=num_perm)
+    for d in data.iloc[i]['article']:
+        mh.update(d.encode('utf8'))
+    mh_list.append(mh)
+
+# Default, use optimizer in constructor
+threshold = 0.8
+lsh = MinHashLSH(threshold=threshold, num_perm=num_perm)
+# Params adjusts the number of bands and size of each band (has to be smaller than num_perms)
+# lsh = MinHashLSH(threshold=threshold, num_perm=num_perm, params=(1,256))
+
+for i in range(0, len(mh_list)):
+    lsh.insert('doc'+str(i), mh_list[i])
+
+count = 0
+for i in range(0, len(mh_list)):
+    results = lsh.query(mh_list[i])
+    if len(results) > 1:
+        print("Similarity > ", threshold, "for query doc", i, ": ", results)
+        count += len(results) - 1
+        #TODO: save as csv in format specified
+
+print("# duplicates found for all queries: ", count)
